1449d5e8c233a84a1a2dc04eaa8a5c225f71e21e
Victor Miguel Vilela Fernandes <113549997+victormiguelVF@users.noreply.github.com>
2022-11-03 15:54:22 -0300

    Projeto SPA

---
+++ js/index.js
@@ -0,0 +1,11 @@
+import {Router} from './router.js'
+
+const router = new Router()
+router.add('/', "/pages/home.html")
+router.add('/universo', "/pages/universo.html")
+router.add('/exploracao', "/pages/exploracao.html")
+
+router.handle()
+
+window.onpopstate = () => router.handle()
+window.route = () => router.route()
